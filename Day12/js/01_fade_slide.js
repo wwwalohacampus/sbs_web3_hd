150642445553306c13779e0bb267483f5e5233ed
luke-youngmin-cho <83196845+luke-youngmin-cho@users.noreply.github.com>
2021-12-17 12:30:42 +0900

    수정

--- Day12/js/01_fade_slide.js
+++ Day12/js/01_fade_slide.js
@@ -8,7 +8,7 @@ $(function() {
 
     setInterval( function() {
 
-        // 슬라이드 박스의 첫 번재 자식요소                    // 1            2
+        // 슬라이드 박스의 첫 번째 자식요소                    // 1            2
         $('#slide > div:first').fadeOut(2000)               // 1번째 사진   
                                .next().fadeIn(2000)         // 2번째 사진
                                .end().appendTo('#slide')    // 1번째 사진
@@ -21,7 +21,7 @@ $(function() {
 // .next()              : 선택한 요소의 다음 요소를 선택하는 메서드
 // .fadeIn(1000)        : 1000ms 동안 서서히 나타나게 하는 메서드
 // .end()               : 현재 선택한 요소의 이전 요소를 선택하는 메서드
-// A.appendTo(B)        : A 를 부모요소()의 마지막 자식 요소로 추가하는 메서드
+// A.appendTo(B)        : A 를 부모요소(B)의 마지막 자식 요소로 추가하는 메서드
 
 
 
