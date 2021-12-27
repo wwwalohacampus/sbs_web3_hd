dd4bcf7a452612ee8d5ee2f0833d4d81d8821092
wwwalohacampus <h850415@naver.com>
2021-12-27 20:03:53 +0900

    1

---
+++ Day15/js/script.js
@@ -0,0 +1,11 @@
+
+$(function() {
+
+    // 스크롤 애니메이션 시작!
+    $('.animate').scrolla({
+        mobile: true,            // 모바일에서 작동 여부
+        once: false,             // 스크롤 애니메이션 1번만 작동
+
+    })
+
+})
